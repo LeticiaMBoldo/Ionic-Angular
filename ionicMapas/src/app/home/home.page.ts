79eab2a342dfc07422149721bf05f6f60a63e7bb
LeticiaMBoldo <60079653+LeticiaMBoldo@users.noreply.github.com>
2020-05-31 13:42:01 -0300

    pegando a posição do mapa
    Pegando a "posição atual" no mapa em tempo real

--- ionicMapas/src/app/home/home.page.ts
+++ ionicMapas/src/app/home/home.page.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+//importar as extensões
 import { ViewChild, ElementRef } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
+//importante declarar uma variavel para o google
 declare var google:any;
 
 @Component({
@@ -10,25 +13,53 @@ declare var google:any;
 })
 export class HomePage {
 
+  //criar variaveis
   map:any;
+  posicaoAtual:any;
+
   @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
 
-  constructor() {}
+  constructor(private geolocation : Geolocation) {}
+
   //pegar uma posição no mapa
-  public showMap(){
-    const location = new google.maps.LatLng(-22.5192128,-48.5687852);
+  public async showMap(){
+    //const location = new google.maps.LatLng(-22.5192128,-48.5687852); posição Fixa
+    //pegar a posição atual do mapa que está na metodo buscarPosicao
+    await this.buscarPosicao();
     const options = {
-      center: location,
+      center: this.posicaoAtual,
       zoom: 15,
       disableDefaultUI: true
     }
+
     this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+
+   const marcador = new google.maps.Marker({
+     position: this.posicaoAtual,
+     map: this.map,
+     title: "minha Posição Atual!",
+     icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
+     animation: google.maps.Animation.BOUNCE
+   });
+
   }
 
   //mostrar a localização no mapa assim que o projeto abrir
   //ciclo de vida do Ionic
   ionViewDidEnter(){
     this.showMap();
+  }
+  
+  public async buscarPosicao(){
+    await this.geolocation.getCurrentPosition().then((posicaoGPS) => {
+      //Pegar a posição no mapa através do GPS do dispositivo.
+      this.posicaoAtual = {
+        lat : posicaoGPS.coords.latitude,
+        lng : posicaoGPS.coords.longitude 
+      }
+     }).catch((error) => {
+       console.log('Erro ao tentar encontrar a localização', error);
+     });
   }
 
 }
