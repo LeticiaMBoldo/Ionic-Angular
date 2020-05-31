8385cbd0a6efcfcda9f703fe2f177579ad5858f4
LeticiaMBoldo <60079653+LeticiaMBoldo@users.noreply.github.com>
2020-05-31 10:52:00 -0300

    utilizando a API da Google
    pegando a localização (latitude e longitude) no mapa usando a API da google

--- ionicMapas/src/app/home/home.page.ts
+++ ionicMapas/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core';
+
+declare var google:any;
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  map:any;
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
+
   constructor() {}
+  //pegar uma posição no mapa
+  public showMap(){
+    const location = new google.maps.LatLng(-22.5192128,-48.5687852);
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+  }
+
+  //mostrar a localização no mapa assim que o projeto abrir
+  //ciclo de vida do Ionic
+  ionViewDidEnter(){
+    this.showMap();
+  }
 
 }
